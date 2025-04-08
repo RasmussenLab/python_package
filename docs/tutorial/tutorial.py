53eeab024b7d138d898a8ba84d7d093d4337c97e
Henry Webel <heweb@dtu.dk>
2025-04-08 12:47:29 +0200

    :art: update tutorial with some more comments

--- docs/tutorial/tutorial.py
+++ docs/tutorial/tutorial.py
@@ -7,15 +7,26 @@
 # %%
 from python_package import hello_world
 
+# %% [markdown]
+# ## Hello World
+# mockup module contains a function which returns a string repeating the 'hello world' string
+# n-times.
+
 # %%
 ret = hello_world(2)
 ret
 
+# %% [markdown]
+# Print the string.
+
 # %%
 print(ret)
+
+# %% [markdown]
+# Inspect the signature.
 
 # %%
 # hello_world?
 
-# %%
+# %% [markdown]
 # Simple example for a recipe showing what the package can do.
